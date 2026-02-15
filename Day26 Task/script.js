57d8fc3732e79f3bc89b73834ec01735dd53814b
Abdur-Rehman <iabdurrehman2.0@gmail.com>
2026-02-15 15:45:31 +0500

    day13,14,15,16,17,18,19,20,21,22,23,24,25,26,27

--- Day26 Task/script.js
+++ Day26 Task/script.js
@@ -2,32 +2,15 @@ let image = document.querySelector(".image")
 let heart = document.querySelector(".image i");
 let t1,t2,t3;
 image.addEventListener("dblclick",()=>{
-     clearTimeout(t1)
-     clearTimeout(t2)
-     clearTimeout(t3)
-
-
-     heart.style.opacity = 0;
-     heart.style.transition = "none";
-     heart.style.transform = "translate(-50%,-50%) scale(0) rotate(-90deg)";
-     heart.style.transition='all ease .2s'
-     heart.offsetHeight
-     
-     
-     t1 = setTimeout(()=>{
-     heart.style.opacity=1;
-     heart.style.transform =' translate(-50%,-50%) scale(1) rotate(0deg)'; 
-     },100)
-     
-     
-     t2 = setTimeout(()=>{
-     heart.style.transition='all ease 0.4s'
-     heart.style.transform =' translate(-50%,-350%)';
-     heart.style.opacity=0; 
-     },550)
-     
-     
-     t3 = setTimeout(()=>{
-     heart.style.transform =' translate(-50%,-50%) scale(0) rotate(-90deg) ';
-     },550+550)
+    heart.style.opacity = 1;
+    heart.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)';
+    setTimeout(()=>{
+     heart.style.transform = 'translate(-50%,-300%) rotate(-60deg)'
+    },500)
+    setTimeout(()=>{
+     heart.style.opacity = 0
+    },701)
+    setTimeout(()=>{
+     heart.style.transform='translate(-50%,-50%) scale(0) rotate(-90deg)'
+    },702)
 })
