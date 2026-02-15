43ae9f7d6f40d3b817f1d64b0904a7fa40985f82
Abdur-Rehman <iabdurrehman2.0@gmail.com>
2026-02-15 14:01:33 +0500

    day13,14,15,16,17,18,19,20,21,22,23,24,25,26

--- Day26 Task/script.js
+++ Day26 Task/script.js
@@ -1,26 +1,33 @@
 let image = document.querySelector(".image")
 let heart = document.querySelector(".image i");
-
+let t1,t2,t3;
 image.addEventListener("dblclick",()=>{
-     async function popUp (){
-           await setTimeout(()=>{
-               heart.style.opacity=1;
-               heart.style.transform =' translate(-50%,-50%) scale(1) rotate(0deg)'; 
-               },100)
-     }
-     popUp();
-     async function moveUp (){
-          await setTimeout(()=>{
-               heart.style.transition='all ease 0.4s'
-               heart.style.transform =' translate(-50%,-350%)';
-               heart.style.opacity=0; 
-               },550)
-     } 
-     moveUp();
-     async function moveBack(){
-          await setTimeout(()=>{
-               heart.style.transform =' translate(-50%,-50%) scale(0) rotate(-90deg) ';
-               },550+550)
-     }
-     moveBack()
+     clearTimeout(t1)
+     clearTimeout(t2)
+     clearTimeout(t3)
+
+
+     heart.style.opacity = 0;
+     heart.style.transition = "none";
+     heart.style.transform = "translate(-50%,-50%) scale(0) rotate(-90deg)";
+     heart.style.transition='all ease .2s'
+     heart.offsetHeight
+     
+     
+     t1 = setTimeout(()=>{
+     heart.style.opacity=1;
+     heart.style.transform =' translate(-50%,-50%) scale(1) rotate(0deg)'; 
+     },100)
+     
+     
+     t2 = setTimeout(()=>{
+     heart.style.transition='all ease 0.4s'
+     heart.style.transform =' translate(-50%,-350%)';
+     heart.style.opacity=0; 
+     },550)
+     
+     
+     t3 = setTimeout(()=>{
+     heart.style.transform =' translate(-50%,-50%) scale(0) rotate(-90deg) ';
+     },550+550)
 })
